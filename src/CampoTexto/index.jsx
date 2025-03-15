b83b6896986bc438793a8b5e854c74d2210f2978
David T. <davidt.info@gmail.com>
2025-03-15 08:00:46 -0300

    Desafio: input estilizado

--- src/CampoTexto/index.jsx
+++ src/CampoTexto/index.jsx
@@ -1,20 +1,41 @@
 import { styled } from "styled-components"
+import search from './search.png'
 
-const InputBusca = styled.input`
-    height: 30px;
-    width: 350px;
-    background-color: transparent;
-    color: white;
-    ::placeholder{
-        color: ;
-    }
-    
+const ContainerEstilizado = styled.div`
+    position: relative;
+    display: inline-block;
+`;
+
+
+const CampoTextoEstilizado = styled.input`
+    height: 56px;
+    padding: 12px 16px;
+    border-radius: 10px;
+    border: 2px solid;
+    border-color: #C98CF1;
+    background: transparent;
+    box-sizing: border-box;
+    width: 566px;
+    color: #D9D9D9;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 20px;
 `
-const CampoTexto = () => {
-    return (
-        <InputBusca placeholder="O que você procura?">
 
-        </InputBusca>
+const IconeLupa = styled.img`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 38px;
+    height: 38px;
+`;
+
+const CampoTexto = (props) => {
+    return (
+        <ContainerEstilizado>
+            <CampoTextoEstilizado placeholder="O que você procura?" {...props} />
+            <IconeLupa src={search} alt="ícone de lupa" />
+        </ContainerEstilizado>
     )
 }
 
