6db91f2c52576a060614afec84f3102794c8e814
David T. <davidt.info@gmail.com>
2025-03-15 22:27:12 -0300

    Desafio: uma tag cheia de estilos

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -8,14 +8,23 @@ const ContainerTags = styled.div`
 `
 
 const BotaoTags = styled.button`
-    background-color: rgba(217, 217, 217, 0.3);
-    color: #FFF;
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
     border-radius: 10px;
-    border-color: #FFF;
-    padding: 10px 8px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #C98CF1;
+    }
 `
 const ParagrafoTags = styled.p `
-    color: #FFF;
+    color: #D9D9D9;
+    font-size: 24px;
+    margin: 0;
 `
 
 
