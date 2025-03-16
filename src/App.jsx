3725f02ad46e7511c2eb9ffbb755d69bb2718797
David T. <davidt.info@gmail.com>
2025-03-15 21:03:16 -0300

    Montando o layout desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,58 @@
-import styled from "styled-components"
-import EstilosGlobais from "./componentes/EstilosGlobais"
-import Cabecalho from "./componentes/Cabecalho"
-import BarraLateral from "./componentes/BarraLateral"
-import Banner from "./componentes/Banner"
-import bannerBackground from './assets/banner.png'
+import styled from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGlobais";
+import Cabecalho from "./componentes/Cabecalho";
+import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+import bannerBackground from "./assets/banner.png";
+import Galeria from "./componentes/Galeria";
 
 const FundoGradiente = styled.div`
-  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
   width: 100%;
   min-height: 100vh;
-`
+`;
+
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`;
+
+const ConteudoGalaria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
 
 function App() {
-  
-
   return (
     <FundoGradiente>
       <EstilosGlobais />
-      <Cabecalho />
-      <BarraLateral />
-      <Banner
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGalaria>
+            <Banner
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-
+            <Galeria />
+          </ConteudoGalaria>
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
-  )
+  );
 }
 
-export default App
+export default App;
