15f03e0b0509229845780a65b4d9cd572b41b3be
David T. <davidt.info@gmail.com>
2025-03-15 22:21:58 -0300

    Desafio: uma tag cheia de estilos - tentativa

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components'
+import tags from './tags.json'
+
+const ContainerTags = styled.div`
+    margin-top: 77px;
+    display: flex;
+    gap: 12px;
+`
+
+const BotaoTags = styled.button`
+    background-color: rgba(217, 217, 217, 0.3);
+    color: #FFF;
+    border-radius: 10px;
+    border-color: #FFF;
+    padding: 10px 8px;
+`
+const ParagrafoTags = styled.p `
+    color: #FFF;
+`
+
+
 const Tags = () => {
-    return <p>Tags</p>
+    return <ContainerTags>
+        <ParagrafoTags>Busque por tags:</ParagrafoTags>
+        {tags.map(tag => <BotaoTags key={tag.id}>{tag.titulo}</BotaoTags>)}
+    </ContainerTags> 
 }
 
 export default Tags
